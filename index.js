391f0ca68887070995d6c2eea262c918b429de3e
Matthew Sandfrey <msandfrey@sbcglobal.net>
2017-10-04 20:17:34 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ function destructivelyRemoveLastKitten(){
   kittens.pop()
 }
 function destructivelyRemoveFirstKitten(){
-  kittens.shift
+  kittens.shift()
 }
 function appendKitten(name){
   return [...kittens, name]
